369eebbf4c9f76d24a1aaada72523c8043fc4306
Stasiperez85 <Stasiperez85@gmail.com>
2022-08-10 21:55:06 -0600

    add javascript and have quotes appear

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+var quoteArray = [];
+var index = 0;
+var textPositon = 0;
+var flag = true;
+var destination = document.getElementById("typedtext");
 
+window.addEventListener('load', typewriter);
+
+function loadQuote() {
+    const url = "https://api.quotable.io/random";
+
+    fetch(url)
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                console.log(response.status);
+        })
+
+        .then(data => {
+            quoteArray[index] = data.content;
+        })
+}
+
+function typewriter() {
+    if (flag) {
+        loadQuote();
+        quoteArray[index] += '';
+        flag = false;
+    }
+    destination.innerHTML = quoteArray[index].substring(0, textPositon) + '<span>\u25AE<span>';
+
+    if (textPositon++ != quoteArray[index].length) {
+        setTimeout('typewriter()', 100);
+    }
+    else {
+        quoteArray[index] = ' ';
+        setTimeout('typewriter()', 3000);
+        textPositon = 0;
+        flag = true;
+
+    }
+}
